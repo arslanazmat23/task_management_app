f01d20c35ae05a91855aeab4e10e5d999817bb9f
arslanazmat23 <bcsm-f19-461@superior.edu.pk>
2023-05-01 11:34:26 +0500

    Update Day 3

--- lib/widgets/TaskTimeLine.dart
+++ lib/widgets/TaskTimeLine.dart
@@ -1,27 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:timeline_tile/timeline_tile.dart';
 
-class TaskTimeLine  extends StatelessWidget {
+class TaskTimeLine extends StatelessWidget {
   final Map<String, dynamic> detail;
-  TaskTimeLine (this.detail);
+  TaskTimeLine(this.detail);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      padding: EdgeInsets.symmetric(horizontal: 15
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      child: Row(
+        children: [
+          _buildTimeLine(detail['tlColor']),
+          Expanded(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                Text(
+                  detail['time'],
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 15,
+                  ),
+                ),
+                detail['title'].isNotEmpty
+                    ? _buildCard(
+                        detail['bgColor'],
+                        detail['title'],
+                        detail['slot'],
+                      )
+                    : _buildCard(Colors.white, '', '')
+              ]))
+        ],
       ),
-
-     child: Row(), 
-
     );
   }
-  Widget _buildTimeLine(Color color){
+
+  Widget _buildCard( Color bgColor, String title, String slot){
+      return Container(
+        width: 250,
+        height: 100,
+        decoration: BoxDecoration(
+          color: bgColor,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10),
+            bottomLeft: Radius.circular(10),
+            bottomRight: Radius.circular(10),
+          )
+        ),
+
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(padding: const EdgeInsets.all(8.0),
+            child: Text(
+                title,
+                style: TextStyle(
+                  color: Colors.grey.shade900,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+
+            ),
+            ),
+            Padding(padding: const EdgeInsets.all(8.0),
+            child: Text(
+              slot,
+              style: TextStyle(
+                color: Colors.grey.shade900,
+                fontSize: 18,
+                fontWeight: FontWeight.w500,)
+            ),)
+          ],
+        ),
+      );
+      
+  }
+
+  Widget _buildTimeLine(Color color) {
     return Container(
       width: 20,
       height: 115,
-      decoration: BoxDecoration(
-        color: color,
-        shape: BoxShape.circle,
+      child: TimelineTile(
+        alignment: TimelineAlign.manual,
+        lineXY: 0,
+        indicatorStyle: IndicatorStyle(
+            indicatorXY: 0,
+            width: 20,
+            indicator: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(
+                  color: color,
+                  width: 5,
+                ),
+                shape: BoxShape.circle,
+              ),
+            )),
+        afterLineStyle: LineStyle(color: color, thickness: 5),
+        isFirst: true,
       ),
     );
   }
